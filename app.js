75a6d752a71c76bdc11cbcfd80ef969e5b605cae
AngelaC123 <angelarare@gmail.com>
2022-04-03 13:41:04 +0800

    Add index.hbs, new.hbs and edit.hbs

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const { engine } = require('express-handlebars')
 const PORT = 3000
 const app = express()
 
+app.use(express.static('public'))
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/new', (req, res) => {
+  res.render('new')
+})
+
+app.get('/edit', (req, res) => {
+  res.render('edit')
 })
 
 app.listen(PORT, () => {
