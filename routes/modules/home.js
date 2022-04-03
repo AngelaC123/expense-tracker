ddfa05a7b6759612cda2dae09c2fadb9471f02f0
AngelaC123 <angelarare@gmail.com>
2022-04-03 21:04:31 +0800

    Modify display index page function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,27 +4,22 @@ const router = express.Router()
 const Category = require('../../models/category')
 const Record = require('../../models/record')
 
-let categoriesDisplay = []
-let totalAmount = 0
 
 router.get('/', (req, res) => {
-
   Category.find()
     .lean()
-    .then(categories => {
-      categoriesDisplay = categories
-    })
-
-  return Record.find()
-    .lean()
-    .then(record => {
-      let totalAmount = 0
-      record.forEach(data => totalAmount += data.amount)
-      res.render('index', { record, category: categoriesDisplay, totalAmount })
+    .then(category => {
+      return Record.find()
+        .lean()
+        .then(record => {
+          let totalAmount = 0
+          record.forEach(data => totalAmount += data.amount)
+          res.render('index', { record, category, totalAmount })
+        })
     })
     .catch(err => console.log(err))
-
-
-
 })
+
+
+
 module.exports = router
