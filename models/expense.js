9b6679e6b3bcc114a4ca7c856d1ba650f9addf7b
AngelaC123 <angelarare@gmail.com>
2022-04-03 14:20:30 +0800

    Add expense schema and category schema

---
+++ models/expense.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const expenseSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  }
+})
+
+module.exports = mongoose.model('expenseSchema', Expense)
