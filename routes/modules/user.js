c2810d39240078cc4604bc9606cd5ba3d2717fb8
AngelaC123 <angelarare@gmail.com>
2022-04-04 11:56:41 +0800

    Add register function

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
+
+const User = require('../../models/user')
 
 // Login
 router.get('/login', (req, res) => {
@@ -18,6 +21,35 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
 
+  const { email, name, password, confirmPassword } = req.body
+  if (!email || !name || !password || !confirmPassword) {
+    console.log('Email, name, password and confimr password are all required!')
+    return res.render('register', { email, name, password, confirmPassword })
+  }
+
+  if (password !== confirmPassword) {
+    console.log('password and confimPassword do not match')
+    return res.render('register', { email, name, password, confirmPassword })
+  }
+
+  return User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('The email has already registered!')
+        return res.render('register', { email, name, password, confirmPassword })
+      }
+
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => {
+          User.create({ email, name, password: hash })
+          console.log('user account created!')
+        })
+        .then(() => res.redirect('/user/login'))
+    })
+
+    .catch(err => console.log(err))
 })
 
 
