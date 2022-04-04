274b00383b9fac43dad2984b07f098a4f224fd14
AngelaC123 <angelarare@gmail.com>
2022-04-04 14:46:17 +0800

    Modify categorySeeder

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,10 +1,32 @@
-const category = require('../../category.json').categoy
 const Category = require('../category')
 
 const db = require('../../config/mongoose')
 
+const SEED_CATEGORIES = [
+  {
+    "name": "家居物業",
+    "icon": "fa-solid fa-house"
+  },
+  {
+    "name": "交通出行",
+    "icon": "fa-solid fa-van-shuttle"
+  },
+  {
+    "name": "休閒娛樂",
+    "icon": "fa-solid fa-face-grin-beam"
+  },
+  {
+    "name": "餐飲食品",
+    "icon": "fa-solid fa-utensils"
+  },
+  {
+    "name": "其他",
+    "icon": "fa-solid fa-pen"
+  }
+]
+
 db.once('open', () => {
-  return Promise.all(Array.from(category, (data, i) => {
+  return Promise.all(Array.from(SEED_CATEGORIES, (data, i) => {
     Category.create({ name: data.name, iconClassList: data.icon })
   }))
     .then(() => {
